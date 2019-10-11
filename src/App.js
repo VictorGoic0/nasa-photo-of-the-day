4f4a45609faf865f7aad47a8a4c520ed248ac6ef
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-10-10 20:49:55 -0400

    Removing default styles, pulling in Card component from components folder. Basic request made in useEffect hook. A loader will render when the request is being made. If the request errors out, an error message will be rendered.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import Card from "./components/Card";
+import Error from "./components/Error";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  const [response, setResponse] = useState({});
+  const [error, setError] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  useEffect(() => {
+    setIsFetching(true);
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res);
+        setResponse({ ...res.data });
+        setIsFetching(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setError(err.response);
+        setIsFetching(false);
+      });
+  }, []);
+  if (error) {
+    return <Error error={error} />;
+  } else if (isFetching) {
+    return <Loader type="Oval" color="#0a4e8a" height={120} width={80} />;
+  } else {
+    return (
+      <div className="App">
+        <Card apod={response} />
+      </div>
+    );
+  }
 }
 
 export default App;
